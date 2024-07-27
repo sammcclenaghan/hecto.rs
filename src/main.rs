f3d3d91c9283c6c4ac73a7985bb549751b2c68ed
Sam McClenghan <sam@aream.ca>
2024-07-27 13:42:12 -0600

    Looking into characters

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ use std::io::{self, Read};
 fn main() {
     enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
-        let ch = b.unwrap() as char;
+        let b = b.unwrap();
+        let ch = b as char;
+        if ch.is_control() {
+            println!("This is a control character \r")
+        } else {
+            println!("This is a normal character \r")
+        }
         if ch == 'q' {
             disable_raw_mode().unwrap();
             break;
