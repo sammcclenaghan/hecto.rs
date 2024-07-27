6366bbff61266fe509d03755511658b7a71002c7
Sam McClenghan <sam@aream.ca>
2024-07-27 12:59:39 -0600

    Added raw mode and easily quiting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let ch = b.unwrap() as char;
+        if ch == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+        println!("{}", ch);
+    }
 }
